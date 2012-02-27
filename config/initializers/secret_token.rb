b401fa6d0d3d029668d1572c09f1f95ade57c696
SWAPNIL <swapnil@ubuntu.(none)>
2012-02-27 10:52:42 +0530

    initialImport

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp5::Application.config.secret_token = 'cf9146d52cafe617b2e7b06174e9240101bb4e20c14d153206c1dde49692c856d681ec7b2a9e5cb853145a03d6a24b0e21f391b92429030f8ffda2c4c107934e'
